c5ce845fbf41d6ab289a7b20cbb4d27075a9331f
yasurihajime <kenta.yuna.yoshida@gmail.com>
2026-06-15 00:39:07 +0900

    Update and rename main.java to FourArithmeticOperations.java

--- Java練習記録/練習群/FourArithmeticOperations.java
+++ Java練習記録/練習群/FourArithmeticOperations.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class main {
-    public static void main(String[] args) {
+    public static void FourArithmeticOperations(String[] args) {
         Scanner sc=new Scanner(System.in);
 
         //入力要素数
